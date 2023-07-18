8f1c84e64e6e9a5ae7dd8c5a26338973d22fab10
ram <stharamkumar3303@gmail.com>
2023-07-18 07:17:38 +0545

    images added and config edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img
+        src="/background.jpg"
+        alt=""
+        style={{ objectFit: "cover", width: "100svw", height: "100svh" }}
+      />
+    </div>
+  );
 }
 
 export default App;
