b55ec50f221614eb5d5cba9eb4e089e9c699381e
ram <stharamkumar3303@gmail.com>
2023-07-18 08:19:16 +0545

    logic implemeted

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+export const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  questionNr,
+  userAnswer,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
